ac703d31fdc901c2cdfe72946e0a6e587868b6b8
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-10-24 12:23:24 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const path = require('path')
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Router
+app.use('/user',require('./Server/router/userRouter'))
+app.use('/api',require('./Server/router/api'))
+
+
+
+
+//connect mongoDB
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI,{
+
+    useCreateIndex:true,
+    useFindAndModify:false ,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, err => {
+    if(err) throw err;
+    console.log('Connected mongoDB')
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
+
+
+app.use('/', (req,res) => {
+    res.json({msg: 'This is Server page!!'})
+});
+
+const Port = process.env.Port || 8000
+app.listen(Port, () => {
+    console.log('Server is running at',Port)
+});
